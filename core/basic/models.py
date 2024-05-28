7346aee27547851c233a88f89ad92ef707e352d3
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-05-29 02:14:50 +0530

    CRUD operation

--- core/basic/models.py
+++ core/basic/models.py
@@ -1,3 +1,18 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+#Create
+class Student(models.Model):
+    #id = models.AutoField()  #automatically added by django if not defined and it is the primary key
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(blank=True)
+    address = models.TextField()
+
+
+class Car(models.Model):
+    name=models.CharField(max_length=100)
+    speed=models.IntegerField(default=100)
+
+    def __str__(self) -> str:
+        return self.name
